c2eb219c0eb289f86e8948b7482982fd3c34dc95
ladynazgool <ladynazgool@gmail.com>
2023-12-08 17:01:10 +0400

    Task 58 added

---
+++ Task-2_58/Program.cs
@@ -0,0 +1,35 @@
+﻿// Написать программу упорядочивания по убыванию элементов каждой строки двумерного массива
+
+Console.Clear();
+System.Console.WriteLine();
+
+int[,] CreateMatrix(int row, int col, int min, int max)
+{
+    int[,] matrix = new int[row, col];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(min, max);
+        }
+    }
+    return matrix;
+}
+
+int[,] SortDesc(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            int minPosition = j;
+            for (int k = j + 1; k < matrix.GetLength(1); k++)
+            {
+                if (matrix[i, k] > matrix[i, minPosition]) minPosition = k;
+            }
+            int tmp = matrix[i, j];
+            matrix[i, j] = matrix[i, minPosition];
+            matrix[i, minPosition] = tmp;
+        }
+    }
+}
