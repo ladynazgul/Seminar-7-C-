6934071e28cebeacd9d93001d29ea283d2b7edb4
ladynazgool <ladynazgool@gmail.com>
2024-04-05 11:30:17 +0400

    Task-4_60_1 finished

---
+++ Task-4_60_1/Program.cs
@@ -0,0 +1,20 @@
+﻿// Составить частотный словарь элементов одномерного массива
+
+int[] array = { 1, 2, 3, 4, 4, 5, 6, 7 };
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] != -1)
+    {
+        int n = array[i];
+        int count = 1;
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if (array[j] == n)
+            {
+                count = count + 1;
+                array[j] = -1;
+            }
+        }
+       Console.WriteLine($"{array[i]} встречается {count} раз");
+    }
+}
